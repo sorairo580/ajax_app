dc621d9f5edb3033f62fb58fc6126f1c696d4003
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-08-15 22:55:43 +0900

    データベースの削除を追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rake db:migrate      [Ctrl + /]でコメントアウト（複数行は[Shift+Alt+a]）
+DISABLE_DATABASE_ENVIROMENT_CHECK=1 bundle exec rake db:migrate:reset
