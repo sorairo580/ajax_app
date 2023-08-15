ed17937c96a75b0415a81544f5fa0a8f7d1c97f9
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-08-16 01:23:07 +0900

    コメントアウトし忘れ

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate      [Ctrl + /]でコメントアウト（複数行は[Shift+Alt+a]）
+bundle exec rake db:migrate     # [Ctrl + /]でコメントアウト（複数行は[Shift+Alt+a]）
